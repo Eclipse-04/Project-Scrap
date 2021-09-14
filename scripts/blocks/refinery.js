e17ad4157308699255c23504985e1a5d7459e394
Eclipse <43229798+vinhsad@users.noreply.github.com>
2021-09-15 06:07:05 +0700

    Update refinery.js

--- scripts/blocks/refinery.js
+++ scripts/blocks/refinery.js
@@ -2,7 +2,3 @@ const Refinery = extend( GenericCrafter, "refinery", {});
 Refinery.drawer = new DrawRotator();
 const bigKiln = extend( AttributeCrafter, "meta-kilnii", {});
 bigKiln.drawer = new DrawSmelter();
-const sulExtractor = extend( GenericCrafter, "sulfur-extractor", {});
-sulExtractor.drawer = new DrawMixer();
-const acidSulfurMixer = extend( LiquidConverter, "acid-sulfuric-mixer", {});
-acidSulfurMixer.drawer = new DrawMixer();
