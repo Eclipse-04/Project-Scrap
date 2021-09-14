0006b1f26d1feef320e78649dec4a4b8b300e8f9
Eclipse <43229798+vinhsad@users.noreply.github.com>
2021-09-15 05:30:58 +0700

    Updated LiquidDrawing for Sulfur Machines

--- scripts/blocks/refinery.js
+++ scripts/blocks/refinery.js
@@ -2,3 +2,7 @@ const Refinery = extend( GenericCrafter, "refinery", {});
 Refinery.drawer = new DrawRotator();
 const bigKiln = extend( AttributeCrafter, "meta-kilnii", {});
 bigKiln.drawer = new DrawSmelter();
+const sulExtractor = extend( AttributeCrafter, "sulfur-extractor", {});
+sulExtractor.drawer = new DrawLiquid();
+const acidSulfurMixer = extend( AttributeCrafter, "acid-sulfuric-mixer", {});
+acidSulfurMixer.drawer = new DrawLiquid();
