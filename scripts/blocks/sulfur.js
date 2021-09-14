cde95b83f8e0428709c991c61ae609d36c1cf98a
uujuju <84468589+uujuju1@users.noreply.github.com>
2021-09-14 20:44:35 -0300

    Update sulfur.js

--- scripts/blocks/sulfur.js
+++ scripts/blocks/sulfur.js
@@ -7,14 +7,14 @@ const SulfurExtractor = extend(GenericCrafter, "sulfur-extractor", {
 		this.GlassRegion = Core.atlas.find("industrial-sulfur-extractor-top");
 	}
 });
-SulfurExtractor.buildType = () => extend(GenericCrafter.GenericCrafterBuild, refiner, {
+SulfurExtractor.buildType = () => extend(GenericCrafter.GenericCrafterBuild, SulfurExtractor, {
 	draw(tile){
 		let inputLiquid = SulfurExtractor.consumes.get(ConsumeType.liquid).liquid;
 
 		Draw.rect(SulfurExtractor.BottomRegion, this.x, this.y, 0);
 
 		Draw.color(Liquids.oil.color);
-		Draw.alpha(this.liquids.get(inputLiquid) / refiner.liquidCapacity);
+		Draw.alpha(this.liquids.get(inputLiquid) / SulfurExtractor.liquidCapacity);
 		Draw.rect(SulfurExtractor.liquidRegion, this.x, this.y);
 
 		Draw.alpha(1);
@@ -32,14 +32,14 @@ const AcidMixer = extend(GenericCrafter, "acid-sulfuric-mixer", {
 		this.GlassRegion = Core.atlas.find("industrial-acid-sulfuric-extractor-top");
 	}
 });
-AcidMixer.buildType = () => extend(GenericCrafter.GenericCrafterBuild, refiner, {
+AcidMixer.buildType = () => extend(GenericCrafter.GenericCrafterBuild, AcidMixer, {
 	draw(tile){
 		let inputLiquid = AcidMixer.consumes.get(ConsumeType.liquid).liquid;
 
 		Draw.rect(AcidMixer.BottomRegion, this.x, this.y, 0);
 
 		Draw.color(Liquids.oil.color);
-		Draw.alpha(this.liquids.get(inputLiquid) / refiner.liquidCapacity);
+		Draw.alpha(this.liquids.get(inputLiquid) / AcidMixer.liquidCapacity);
 		Draw.rect(AcidMixer.liquidRegion, this.x, this.y);
 
 		Draw.alpha(1);
