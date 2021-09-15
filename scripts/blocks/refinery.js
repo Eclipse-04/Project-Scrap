d1cd782521dbacae86734a5775f522e07c54674a
Eclipse <43229798+vinhsad@users.noreply.github.com>
2021-09-16 04:26:53 +0700

    Update refinery.js

--- scripts/blocks/refinery.js
+++ scripts/blocks/refinery.js
@@ -2,3 +2,7 @@ const Refinery = extend( GenericCrafter, "refinery", {});
 Refinery.drawer = new DrawRotator();
 const bigKiln = extend( AttributeCrafter, "meta-kilnii", {});
 bigKiln.drawer = new DrawSmelter();
+const smolSteel = extend( GenericCrafter, "steel-smelter", {});
+smolSteel.drawer = new DrawSmelter();
+const bigSteel = extend( AttributeCrafter, "steel-forge", {});
+bigSteel.drawer = new DrawSmelter();
