e35664b16abeb856b30137b344fc830f14722edf
Eclipse <43229798+vinhsad@users.noreply.github.com>
2021-09-20 14:35:50 +0700

    Update refinery.js

--- scripts/blocks/refinery.js
+++ scripts/blocks/refinery.js
@@ -13,4 +13,4 @@ silicglass.drawer = pain
 const quartRefine = extend( GenericCrafter, "quartz-refinery", {});
 const spritePain = new DrawSmelter
 spritePain.spinSpeed = 5
-quartRefine.drawer = new DrawRotator();
+quartRefine.drawer = spritePain
