f72fa8017649a438316b4413b4a0b6c4e9f1c0da
Eclipse <43229798+vinhsad@users.noreply.github.com>
2021-09-22 15:54:14 +0700

    Update sulfur.js

--- scripts/blocks/sulfur.js
+++ scripts/blocks/sulfur.js
@@ -47,3 +47,28 @@ AcidMixer.buildType = () => extend(GenericCrafter.GenericCrafterBuild, AcidMixer
 		Draw.rect(AcidMixer.GlassRegion, this.x, this.y, 0);
 	}
 });
+const ThiccQuartzRefinery = extend(GenericCrafter, "quartz-refinery-a", {
+	load(){
+		this.super$load();
+		this.region = Core.atlas.find("industrial-quartz-refinery-a");
+		this.liquidRegion = Core.atlas.find("industrial-quartz-refinery-a-liquid");
+		this.rOtaE = Core.atlas.find("industrial-quartz-refinery-a-rotator");
+		this.GlassRegion = Core.atlas.find("industrial-quartz-refinery-a-top");
+	}
+});
+ThiccQuartzRefinery.buildType = () => extend(GenericCrafter.GenericCrafterBuild, ThiccQuartzRefinery, {
+	draw(tile){
+		let inputLiquid = ThiccQuartzRefinery.consumes.get(ConsumeType.liquid).liquid;
+
+		Draw.rect(ThiccQuartzRefinery.region, this.x, this.y, 0);
+
+		Draw.color(Liquids.oil.color);
+		Draw.alpha(this.liquids.get(inputLiquid) / SulfurExtractor.liquidCapacity);
+		Draw.rect(ThiccQuartzRefinery.liquidRegion, this.x, this.y);
+
+		Draw.alpha(1);
+		Draw.color();
+		Draw.rect(ThiccQuartzRefinery.rOtaE, this.x, this.y, this.totalProgress * 3);
+		Draw.rect(ThiccQuartzRefinery.GlassRegion, this.x, this.y, 0);
+	}
+});
