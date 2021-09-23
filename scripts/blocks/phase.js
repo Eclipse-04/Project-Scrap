51d2f5645cbc59b651ede246522b774624a42e5e
Eclipse <43229798+vinhsad@users.noreply.github.com>
2021-09-23 10:24:24 +0700

    Add files via upload

---
+++ scripts/blocks/phase.js
@@ -0,0 +1,14 @@
+const thiccPhase = extend(GenericCrafter, "phase-constructor", {
+	load(){
+		this.super$load();
+		this.region = Core.atlas.find("industrial-phase-constructor");
+		this.point = Core.atlas.find("industrial-phase-constructor-bar");
+		this.bar = Core.atlas.find("industrial-phase-constructor-point");
+	}
+});
+thiccPhase.buildType = () => extend(GenericCrafter.GenericCrafterBuild, thiccPhase, {
+	draw(tile){
+		Draw.rect(thiccPhase.region, this.x, this.y, 0);
+        Draw.rect(thiccPhase.bar, this.x + Math.sin(this.totalProgress * 360))
+	}
+});
