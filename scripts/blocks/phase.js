744318a60c528375c25771ae7a113fe001efaafe
Eclipse <43229798+vinhsad@users.noreply.github.com>
2021-09-24 20:27:30 +0700

    Update phase.js

--- scripts/blocks/phase.js
+++ scripts/blocks/phase.js
@@ -2,13 +2,13 @@ const thiccPhase = extend(GenericCrafter, "phase-constructor", {
 	load(){
 		this.super$load();
 		this.region = Core.atlas.find("industrial-phase-constructor");
-		this.point = Core.atlas.find("industrial-phase-constructor-bar");
-		this.bar = Core.atlas.find("industrial-phase-constructor-point");
+		this.bar = Core.atlas.find("industrial-phase-constructor-bar");
+		this.point = Core.atlas.find("industrial-phase-constructor-point");
 	}
 });
 thiccPhase.buildType = () => extend(GenericCrafter.GenericCrafterBuild, thiccPhase, {
 	draw(tile){
 		Draw.rect(thiccPhase.region, this.x, this.y, 0);
-        	Draw.rect(thiccPhase.bar, this.x + Angles.trnsx(this.totalProgress * 360, 0.5), this.y, 0);
+        	Draw.rect(thiccPhase.bar, this.x + Angles.trnsx(this.totalProgress * 360, 1), this.y, 0);
 	}
 });
