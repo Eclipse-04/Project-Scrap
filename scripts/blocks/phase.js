0c14e06477c6ea69b52a87c4b466c5134506df75
Eclipse <43229798+vinhsad@users.noreply.github.com>
2021-09-25 21:27:26 +0700

    Update phase.js

--- scripts/blocks/phase.js
+++ scripts/blocks/phase.js
@@ -14,8 +14,8 @@ thiccPhase.buildType = () => extend(GenericCrafter.GenericCrafterBuild, thiccPha
 		Draw.alpha(this.totalProgress)
 		Draw.rect(thiccPhase.phase, this.x, this.y, 0)
 		Draw.alpha(1)
-		Draw.rect(thiccPhase.bar, this.x + Mathf.cos(this.totalProgress, 1, 3), this.y, 0)
-		Draw.rect(thiccPhase.point, this.x + Mathf.cos(this.totalProgress, 1, 3), this.y + Mathf.cos(this.totalProgress, 2, 3), 0)
+		Draw.rect(thiccPhase.bar, this.x + Mathf.cos(this.totalProgress, 0.5, 3), this.y, 0)
+		Draw.rect(thiccPhase.point, this.x + Mathf.cos(this.totalProgress, 0.5, 3), this.y + Mathf.cos(this.totalProgress, 0.5, 3), 0)
         	Draw.rect(thiccPhase.top, this.x, this.y, 0)
 	}
 });
