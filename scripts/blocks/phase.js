7263930090db88ec292bc0e5e988359afa0aebba
Eclipse <43229798+vinhsad@users.noreply.github.com>
2021-09-26 21:55:20 +0700

    Update phase.js

--- scripts/blocks/phase.js
+++ scripts/blocks/phase.js
@@ -18,7 +18,7 @@ phaseConstructor.buildType = () => extend(GenericCrafter.GenericCrafterBuild, ph
 
     Draw.alpha(0.5 + Mathf.absin(this.totalProgress, 10, 0.5));
 
-    Draw.rect(phaseConstructor.phaseRegion, this.x, this.y, this.totalProgress);
+    Draw.rect(phaseConstructor.phaseRegion, this.x, this.y, 0);
 
     Draw.alpha(1);
 
