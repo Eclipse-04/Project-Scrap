bb65c8f651bd6e55441e785a65d80400e15881b3
EclipseTheOldOne <43229798+EclipseTheOldOne@users.noreply.github.com>
2021-10-17 02:14:40 +0700

    help i'm stuck ||at js||

--- scripts/event/collecting.js
+++ scripts/event/collecting.js
@@ -1,7 +1,5 @@
+//listen for the event where a unit is destroyed
 Events.on(UnitDestroyEvent, event => {
     //display toast on top of screen when the unit was a player
-    if(event.unit.isPlayer()){
-      Vars.ui.hudfrag.showToast("Pathetic.");
-    }
-})
-//testing
+    Vars.ui.hudfrag.showToast("Pathetic.");
+  })
