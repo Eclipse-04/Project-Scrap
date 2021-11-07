056e0f15b92a9946479c52d664747eb2348f9d7f
EclipseTheOldOne <43229798+EclipseTheOldOne@users.noreply.github.com>
2021-11-08 06:18:14 +0700

    adding bigger heat gen

--- scripts/blocks/heat-generator.js
+++ scripts/blocks/heat-generator.js
@@ -1,4 +1,3 @@
-
 const heatGenerator = extend(BurnerGenerator, "heat-generator", {
   generateEffect: Fx.none,
   generateFx: Fx.smeltsmoke,
@@ -15,3 +14,18 @@ heatGenerator.buildType = () => extend(BurnerGenerator.BurnerGeneratorBuild, hea
   }
 })
 
+const bigHeatGenerator = extend(BurnerGenerator, "heat-gen-big", {
+  generateEffect: Fx.none,
+  generateFx: Fx.smeltsmoke,
+  getLiquidEfficiency(liquid) {
+    return liquid.temperature
+  }
+});
+
+heatGenerator.buildType = () => extend(BurnerGenerator.BurnerGeneratorBuild, heatGenerator, {
+  update(){
+    this.super$update();
+    
+    if(this.productionEfficiency > 0.0 && this.generateTime - Time.delta <= 0 && Mathf.chance(0.01)) heatGenerator.generateFx.at(this.x + Mathf.range(heatGenerator.generateEffectRnd), this.y + Mathf.range(heatGenerator.generateEffectRnd));
+  }
+})
