06e3ecabad4fc14516e19373b0ab474b76072264
EclipseTheOldOne <43229798+EclipseTheOldOne@users.noreply.github.com>
2021-11-17 10:54:54 +0700

    wat

---
+++ scripts/blocks/big-node.js
@@ -0,0 +1,21 @@
+const bigCoil = extend(PowerNode, "magnetic-coil", {
+    draw(tile) {
+        this.super$draw(tile);
+        Draw.rect(
+            "industrial-magnetic-coil",
+            tile.x,
+            tile.y
+        );
+        Draw.rect(
+            "industrial-magnetic-coil-rotator",
+            tile.x,
+            tile,y,
+            Time.time * 2
+        );
+        Draw.rect(
+            "industrial-magnetic-coil-top",
+            tile.x,
+            tile.y
+        );
+    }
+});
