ab61cccad5eec2981d1e30b1ad047ec2a999a6c9
EclipseTheOldOne <43229798+EclipseTheOldOne@users.noreply.github.com>
2021-11-18 11:59:22 +0700

    aaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,7 @@ require("blocks/refinery");
 require("blocks/heat-generator");
 require("blocks/sulfur");
 require("units/drone");
+require("units/copterC");
 require("blocks/phase");
 //require("event/collecting")
 require("blocks/multicrafter/ammo-factory")
