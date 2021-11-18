f7da0758f186d92df9c5e68afa8cf0c9c0ec1e10
EclipseTheOldOne <43229798+EclipseTheOldOne@users.noreply.github.com>
2021-11-18 07:52:42 +0700

    updated

--- scripts/blocks/big-node.js
+++ scripts/blocks/big-node.js
@@ -1,14 +1,14 @@
-const bigCoil = extend(PowerNode, "magnetic-coil", {
-    draw(tile) {
-        this.super$draw(tile);
+const bigCoil = extend(PowerNode, "magnetic-coil", {});
+bigCoil.buildType = () => extend(PowerNode.PowerNodeBuild, bigCoil, {
+    draw() {
         Draw.rect(
-            "industrial-magnetic-coil", tile.x, tile.y
+            "industrial-magnetic-coil", this.x, this.y
         );
         Draw.rect(
-            "industrial-magnetic-coil-rotator", tile.x, tile,y, Time.time * 2
+            "industrial-magnetic-coil-rotator", this.x, this,y, Time.time * 2
         );
         Draw.rect(
-            "industrial-magnetic-coil-top", tile.x, tile.y
+            "industrial-magnetic-coil-top", this.x, this.y
         );
     }
 });
