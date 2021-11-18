c6d1072316eea19b3e10c9627945ee72c8168e3a
EclipseTheOldOne <43229798+EclipseTheOldOne@users.noreply.github.com>
2021-11-18 10:11:11 +0700

    wat

--- scripts/blocks/big-node.js
+++ scripts/blocks/big-node.js
@@ -1,18 +1,18 @@
 const bigCoil = extend(PowerNode, "magnetic-coil", {
     load() {
-        this.top = Core.atlas.find(this.name + "-top");
-        this.rotator = Core.atlas.find(this.name + "-rotator");
+        this.topRegion = Core.atlas.find(this.name + "-top");
+        this.rotatorRegion = Core.atlas.find(this.name + "-rotator");
     },
     icons(){
         return [
-            this.region, this.rotator, this.top
+            this.region, this.rotatorRegion, this.topRegion
         ];
     }
 });
 bigCoil.buildType = () => extend(PowerNode.PowerNodeBuild, bigCoil, {
     draw() {
         Draw.rect(bigCoil.region, this.x, this.y);
-        Draw.rect(bigCoil.rotator, this.x, this,y, Time.time * 2);
-        Draw.rect(bigCoil.top, this.x, this.y);
+        Draw.rect(bigCoil.rotatorRegion, this.x, this,y, Time.time * 2);
+        Draw.rect(bigCoil.topRegion, this.x, this.y);
     }
 });
