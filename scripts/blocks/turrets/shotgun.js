88e0d47d741618ae71fc5e518e005f5c6bf1c288
EclipseTheOldOne <43229798+EclipseTheOldOne@users.noreply.github.com>
2021-11-20 09:45:12 +0700

    testing more things

--- scripts/blocks/turrets/shotgun.js
+++ scripts/blocks/turrets/shotgun.js
@@ -1,6 +1,7 @@
 let lib = require("ammon-lib");
 const salve = extend(ItemTurret, "salve", {})
 salve.ammo(
+    Items.copper, Bullets.standardCopper,
     Items.scrap, lib.shotGunScrap,
     Items.lead, lib.shotGunLead,
     Items.graphite, lib.shotGunGraphite,
